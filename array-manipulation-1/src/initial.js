7776230c56d5b1bc2d362bb4ec5c09970188d71b
Sanford Huynh <huynhsanford@gmail.com>
2023-07-27 23:48:44 +0000

    Added pseudocode for the function initial(array)

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -1,4 +1,15 @@
 /* exported initial */
+
+/**
+ * Define a function named "initial", that accepts one argument ('array')
+ * Declare a const called 'initalArray' and set it's value to an empty array
+ * Begin a loop that iterates through the array where
+ *  For each index in the array
+ *  Push the value at that array's index into the initialArray until 2nd last element
+ * Return the value of the initialArray
+ *
+ *
+ */
 function initial(array) {
   const initialArray = [];
   for (let i = 0; i < array.length - 1; i++) {
