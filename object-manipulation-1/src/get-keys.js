1deca2a5bbd9e08aedf986b02f74efaa6b76a404
Sanford Huynh <huynhsanford@gmail.com>
2023-07-27 23:53:23 +0000

    Added the pseudocode for the function get-keys

--- object-manipulation-1/src/get-keys.js
+++ object-manipulation-1/src/get-keys.js
@@ -1,4 +1,12 @@
 /* exported getKeys */
+/**
+ * Define a function named "getKeys", that accepts one argument 'object'
+ * Create a const variable named 'keys' and assign it an empty array
+ * Begin a loop that iterates through the object
+ *  For iterating through the object properties
+ *  Push the property into the keys array
+ * Return the 'keys' variable
+ */
 function getKeys(object) {
   const keys = [];
 
