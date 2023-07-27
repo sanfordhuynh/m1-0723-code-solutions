30922cd0d09d6006d2b7523b40b501955279a5be
Sanford Huynh <huynhsanford@gmail.com>
2023-07-27 23:35:01 +0000

    Fixed the conditions of function categorizeAcidity to make it stay within 0-7 or 7-14

--- javascript-conditionals/main.js
+++ javascript-conditionals/main.js
@@ -59,9 +59,9 @@ function isOldEnoughToDrinkAndDrive(person) {
 function categorizeAcidity(pH) {
   if (pH === 7) {
     return 'neutral';
-  } else if (pH < 7 && pH > 0) {
+  } else if (pH < 7 && pH >= 0) {
     return 'acid';
-  } else if (pH > 7 && pH < 14) {
+  } else if (pH > 7 && pH <= 14) {
     return 'base';
   } else {
     return 'invalid pH level';
